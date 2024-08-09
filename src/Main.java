5e60e0fb1bc5ad67b98e8d03e9c5b59934ecb25d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-09 10:39:38 +0300

    add two more labels

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,22 @@ public class Main {
         //                   third dimension for positioning components
         //                    ex. depth, Z-index
 
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED);
+        label1.setBounds(50,50,200,200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.GREEN);
+        label2.setBounds(100,100,200,200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.BLUE);
+        label3.setBounds(150,150,200,200);
+
+
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setBounds(0,0,500,500);
 
